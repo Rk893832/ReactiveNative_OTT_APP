ee8cda79714e1597883b4db2d1307f60917aea48
rohit <rohitkumar893832@gmail.com>
2023-08-25 12:46:21 +0530

    updated code again

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -10,17 +10,26 @@ import SidebarScreen from '../screens/Sidebar/SidebarScreen';
 
 const Stack = createNativeStackNavigator();
 
+
 const AppNavigator = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
+                <Stack.Screen name="Signup" component={SignupScreen} options={{
+                    headerTitleStyle: commonHeaderTitleStyle,
+                }} />
                 <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome' }} />
-                <Stack.Screen name="Signup" component={SignupScreen} />
                 <Stack.Screen name="Login" component={LoginScreen} />
                 <Stack.Screen name="Sidebar" component={SidebarScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
+};
+
+const commonHeaderTitleStyle = {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'skyblue', // Customize the color
 };
 
 export default AppNavigator;
